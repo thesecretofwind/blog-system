768bbd93e9b039eba76f910841d31feb6968fb04
chengguang.li <Chengguang.Li@vertiv.com>
2024-03-22 18:03:15 +0800

    fix：配置静态文件路径

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // 配置静态路径
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('public', {
+    prefix: '/static/',
+  });
   await app.listen(3000);
 }
 bootstrap();
